eb029f3127f2f267508b7901066e95504e38e4db
Pablo Morillas Plaza <pmorillasplaza.pmp@gmail.com>
2023-03-20 20:12:22 +0100

    DISSMISS IMPLEMENTADO
    Implementada la función dismiss con una lista de prueba

--- lib/pantallas/pagina_principal.dart
+++ lib/pantallas/pagina_principal.dart
@@ -8,6 +8,9 @@ class PaginaPrincipal extends StatefulWidget {
 }
 
 class _PaginaPrincipalState extends State<PaginaPrincipal> {
+  // TEST LIST
+  final items = List<String>.generate(30, (index) => 'Item #${index + 1}');
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -40,9 +43,33 @@ class _PaginaPrincipalState extends State<PaginaPrincipal> {
             delegate: SliverChildBuilderDelegate(
               // The builder function returns a ListTile with a title that
               // displays the index of the current item.
-                  (context, index) => ListTile(title: Text('Item #$index')),
-              // Builds 1000 ListTiles
-              childCount: 100,
+                  (context, index) {
+                    final item = items[index];
+                    return Dismissible(
+                      // Each Dismissible must contain a Key. Keys allow Flutter to
+                      // uniquely identify widgets.
+                      key: Key(item),
+                      // Provide a function that tells the app
+                      // what to do after an item has been swiped away.
+                      onDismissed: (direction) {
+                        // Remove the item from the data source.
+                        setState(() {
+                          items.removeAt(index);
+                        });
+
+                        // Then show a snackbar.
+                        ScaffoldMessenger.of(context)
+                            .showSnackBar(SnackBar(content: Text('$item dismissed')));
+                      },
+                      // Show a red background as the item is swiped away.
+                      background: Container(color: Colors.red),
+                      child: ListTile(
+                        title: Text(item),
+                      ),
+                    );
+                  },
+              // Builds items.length ListTiles
+              childCount: items.length,
             ),
           )
         ],
